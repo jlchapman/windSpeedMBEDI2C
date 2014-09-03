a88c713804445965ec061839869479d75807a3e0
jlchapman <raptor90se@msn.com>
2014-09-03 15:01:37 -0600

    Update demo.py

--- demo.py
+++ demo.py
@@ -11,28 +11,28 @@ from datetime import datetime
 # sensor. (Common implementations are in README.md)
 sensor = BBB_WINDMBED(address=0x52, bus=0)
 
-def handle_interrupt(channel):
+//def handle_interrupt(channel):
     time.sleep(0.003)
-    global sensor
-    if reason == 0x01:
-        print "Just testing 0x01"
+//    global sensor
+//    if reason == 0x01:
+//        print "Just testing 0x01"
 //        sensor.raise_noise_floor()
-    elif reason == 0x04:
-        print "Just testing 0x04"
+//    elif reason == 0x04:
+//        print "Just testing 0x04"
 //        sensor.set_mask_disturber(True)
-    elif reason == 0x08:
+//    elif reason == 0x07:
         now = datetime.now().strftime('%H:%M:%S - %Y/%m/%d')
         distance = sensor.get_distance()
-        print "Just testing 0x08"
+        print "Just testing 0x07"
 //        print "It was " + str(distance) + "km away. (%s)" % now
 //        print ""
 
 pin = 17
 
-GPIO.setup(pin, GPIO.IN)
-GPIO.add_event_detect(pin, GPIO.RISING, callback=handle_interrupt)
+//GPIO.setup(pin, GPIO.IN)
+//GPIO.add_event_detect(pin, GPIO.RISING, callback=handle_interrupt)
 
-print "Waiting for lightning - or at least something that looks like it"
+print "One time through"
 
-while True:
-    time.sleep(1.0)
+//while True:
+ //   time.sleep(1.0)
