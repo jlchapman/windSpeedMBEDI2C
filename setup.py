37aad7e86306e67f7a4db828bb48cb9f60840f60
jlchapman <raptor90se@msn.com>
2014-09-02 19:30:53 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name = 'BBB_WINDMBED',
+      version = '0.0.1',
+      description = 'BeagleBone Black <-I2C-> WindSpeed/Direction communication library',
+      author = 'Jerry Chapman',
+      author_email = 'jerrychapman84@gmail.com',
+      url = 'https://github.com/jlchapman/windSpeedMBEDI2C',
+      packages = ['BBB_WINDMBED'],
+      license = 'GPL',
+)
