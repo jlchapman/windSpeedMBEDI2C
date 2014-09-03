bc85d6b02267a31322b2c14a75df6293f52a7c05
jlchapman <raptor90se@msn.com>
2014-09-02 20:33:02 -0600

    Create __init__.py

---
+++ BBB_WINDMBED/__init__.py
@@ -0,0 +1,8 @@
+"""A module to make communication between the Beaglebone Black and the
+MBED Wind Direction/Wind Speed (over I2C) as easy as possible.
+
+.. moduleauthor:: Jerry Chapman <jerrychapman84@gmail.com>
+
+"""
+
+from BBB_WINDMBED import BBB_WINDMBED
